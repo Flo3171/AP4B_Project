16540b36722d6b31e340c2f4f3f65b81f90f6c25
Fituning <carstar@live.fr>
2021-11-29 13:02:58 +0100

    test
    test

--- src/main/java/helloWord/HelloWord.java
+++ src/main/java/helloWord/HelloWord.java
@@ -2,7 +2,7 @@ package helloWord;
 
 public class HelloWord {
     public static void main(String[] args) {
-        System.out.println("Hello word !");
+        System.out.println("Hello Wejgame.co !");
     }
 
 
